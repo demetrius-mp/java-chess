4c0fd14bc7977c1e572ca9f14a4472bf96a8673d
Demetrius <demetrius.mp789@gmail.com>
2021-07-06 22:22:31 -0400

    Movimentaçao inicial de peças

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Piece;
+import boardgame.Position;
 import chess.pieces.King;
 import chess.pieces.Rook;
 
@@ -22,6 +24,32 @@ public class ChessMatch {
         }
 
         return mat;
+    }
+
+    public ChessPiece performChessMove(ChessPosition sourcePosition, ChessPosition targetPosition) {
+        Position source = sourcePosition.toPosition();
+        Position target = targetPosition.toPosition();
+
+        validateSourcePosition(source);
+
+        Piece capturedPiece = makeMove(source, target);
+
+        return (ChessPiece) capturedPiece;
+    }
+
+    private Piece makeMove(Position source, Position target) {
+        Piece p = this.board.removePiece(source);
+        Piece capturedPiece = this.board.removePiece(target);
+
+        this.board.placePiece(p, target);
+
+        return capturedPiece;
+    }
+
+    private void validateSourcePosition(Position position) {
+        if (!this.board.thereIsAPiece(position)) {
+            throw new ChessException("Não há nenhuma peça na posição de origem.");
+        }
     }
 
     private void placeNewPiece(char column, int row, ChessPiece piece) {
