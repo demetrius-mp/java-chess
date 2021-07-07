a41711d2ec765be528d550623496b878b5da23ce
Demetrius <demetrius.mp789@gmail.com>
2021-07-07 18:58:44 -0400

    fix: removendo exceçao da promoçao

--- src/application/Program.java
+++ src/application/Program.java
@@ -42,7 +42,13 @@ public class Program {
 
                 if (chessMatch.getPromoted() != null) {
                     System.out.print("Escolha uma peça para a promoção (B/N/R/Q): ");
-                    String type = sc.nextLine();
+                    String type = sc.nextLine().toUpperCase();
+
+                    while (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
+                        System.out.println("Valor inválido! Escolha uma peça para a promoção (B/N/R/Q): ");
+                        type = sc.nextLine().toUpperCase();
+                    }
+
                     chessMatch.replacePromotedPiece(type);
                 }
             }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,5 @@
 package chess;
 
-import java.security.InvalidParameterException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -135,7 +134,7 @@ public class ChessMatch {
         }
 
         if (!type.equals("B") && !type.equals("N") && !type.equals("R") & !type.equals("Q")) {
-			throw new InvalidParameterException("Tipo inválido para promoção");
+			return this.promoted;
 		}
 
         Position pos = this.promoted.getChessPosition().toPosition();
